9c3ce2c3b6c8369a8e5a29b5b91d35d99e1fdaad
张子豪 <31848851+AniviaKid@users.noreply.github.com>
2017-12-20 13:03:22 +0800

    增加lab9_v0

---
+++ lab9/lab9_code/app/src/main/java/com/example/asus/lab9/Github.java
@@ -0,0 +1,24 @@
+package com.example.asus.lab9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by ASUS on 2017/12/19.
+ */
+
+public class Github {
+    private String login;//用户
+    private String blog;//用户
+    private int id;//用户
+    private String name;//仓库
+    private String language;//仓库
+    private String description;//仓库
+    public String getLogin(){return login;}
+    public String getBlog(){return blog;}
+    public int getId(){return id;}
+    public String getName(){return name;}
+    public String getLanguage(){return language;}
+    public String getDescription(){return description;}
+}
